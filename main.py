2bb6e22b77e04b6960c0c6325ff182f9b8c13d56
maxrice <mxrice@loyola.edu>
2024-09-17 18:27:34 -0400

    Max Rice did first half of code

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 # Enter code here
 # Read the README for instructions.
 # You must write an algorithm and test cases first!
+
+distance_in_miles = float(input("Enter distance in miles: "))
+miles_per_gallon = float(input("Enter miles per gallon: "))
+gallons_needed = distance_in_miles * miles_per_gallon
+cost_of_gas = 1
+cost_of_trip = gallons_needed * cost_of_gas
